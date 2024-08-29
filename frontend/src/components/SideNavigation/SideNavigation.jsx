eb3009ffbce233f21d807f7f1e36149a8e329ede
Derek Lin <12linderek@gmail.com>
2024-08-28 21:51:45 -0700

    added top nav bar and side nav bar

---
+++ frontend/src/components/SideNavigation/SideNavigation.jsx
@@ -0,0 +1,19 @@
+import './SideNavigation.css'
+import { NavLink } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+
+
+
+function SideNavigation() {
+    return (<>
+        <nav className='sidebar'>
+            <ul>
+                <li><NavLink to="/manage-lessons" className={({ isActive }) => isActive ? "active" : ""}>Manage Lessons</NavLink></li>
+                <li><NavLink to="/manage-reviews" className={({ isActive }) => isActive ? "active" : ""}>Manage Reviews</NavLink></li>
+            </ul>
+        </nav>
+    </>);
+}
+
+export default SideNavigation;
--- frontend/src/router/Layout.jsx
+++ frontend/src/router/Layout.jsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { ModalProvider, Modal } from "../context/Modal";
 import { thunkAuthenticate } from "../redux/session";
 import Navigation from "../components/Navigation/Navigation";
+import SideNavigation from "../components/SideNavigation/SideNavigation";
+import "./Layout.css"
 
 export default function Layout() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
+  const sessionUser = useSelector((state) => state.session.user)
+
   useEffect(() => {
     dispatch(thunkAuthenticate()).then(() => setIsLoaded(true));
   }, [dispatch]);
@@ -16,7 +20,12 @@ export default function Layout() {
     <>
       <ModalProvider>
         <Navigation />
-        {isLoaded && <Outlet />}
+        {isLoaded && (
+          <>
+            {sessionUser && <SideNavigation />} {/* Render SideNavigation if the user is logged in */}
+            <Outlet />
+          </>
+        )}
         <Modal />
       </ModalProvider>
     </>
