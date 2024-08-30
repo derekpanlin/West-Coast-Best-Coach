e0fc0ba5aac4ce085ae42bf2a42c407709dad739
Derek Lin <12linderek@gmail.com>
2024-08-29 19:21:02 -0700

    conditionally render find a coach only after user has been authenticated as logged in

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,19 +1,25 @@
 import { NavLink } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
+import { useSelector } from "react-redux";
 import "./Navigation.css";
 
+
 function Navigation() {
+  const user = useSelector((store) => store.session.user);
+
   return (
     <nav className="nav-container">
       <ul className="nav-list">
         <li className="nav-item">
           <NavLink to="/" className="home-link">West Coast Best Coach</NavLink>
         </li>
-        <li className="nav-item">
-          <NavLink to="/find-a-coach" className="find-a-coach-button">
-            Find a Coach!
-          </NavLink>
-        </li>
+        {user && (
+          <li className="nav-item">
+            <NavLink to="/find-a-coach" className="find-a-coach-button">
+              Find a Coach!
+            </NavLink>
+          </li>
+        )}
         <li className="nav-item profile-button">
           <ProfileButton />
         </li>
