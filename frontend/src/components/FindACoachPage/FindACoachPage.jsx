c7dab1766d0284d51210be07364410e4020c45f2
Derek Lin <12linderek@gmail.com>
2024-08-30 18:00:22 -0700

    added list of cities to find coach, and styled it

--- frontend/src/components/FindACoachPage/FindACoachPage.jsx
+++ frontend/src/components/FindACoachPage/FindACoachPage.jsx
@@ -1,15 +1,27 @@
-import './FindACoachPage.css'
+import { NavLink } from 'react-router-dom';
+import './FindACoachPage.css';
 
 function FindACoachPage() {
-    return (<div>
-        <h1>Pick a City</h1>
-        <div>
-            <h2>NorCal</h2>
-            <h2>SoCal</h2>
-
+    return (
+        <div className="find-coach-container">
+            <h1 className="find-coach-title">Pick a City</h1>
+            <div className="find-coach-sections">
+                <div className="find-coach-section">
+                    <h2 className="find-coach-region">NorCal</h2>
+                    <ul className="city-list">
+                        <li><NavLink to="/san-francisco-coaches">San Francisco</NavLink></li>
+                        <li><NavLink to="/san-jose-coaches">San Jose</NavLink></li>
+                    </ul>
+                </div>
+                <div className="find-coach-section">
+                    <h2 className="find-coach-region">SoCal</h2>
+                    <ul className="city-list">
+                        <li><NavLink to="/irvine-coaches">Irvine</NavLink></li>
+                        <li><NavLink to="/los-angeles-coaches">Los Angeles</NavLink></li>
+                    </ul>
+                </div>
+            </div>
         </div>
-    </div>
-
     );
 }
 
