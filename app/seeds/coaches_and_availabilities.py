0c9fdef3851fea7db48fd62e6370bc69c03ef2ca
Derek Lin <12linderek@gmail.com>
2024-09-24 17:23:41 -0700

    added a bunch of coaches and formatted rate to locale string so it can add commas

--- app/seeds/coaches_and_availabilities.py
+++ app/seeds/coaches_and_availabilities.py
@@ -7,7 +7,7 @@ def seed_coaches_and_availabilities():
             "first_name": "Derek",
             "last_name": "Lin",
             "rate": 100,
-            "bio": "Hello my name is Derek and I am a local Irvine tennis coach with over 20 years of playing experience and over 16 years of teaching experience! I achieved a Top 75 national ranking back in juniors. I was the #1 singles player for the national club team at UC Davis (which is my alma mater and after graduating, I also played on the Irvine Valley College’s 3-Time State Championship Team). I was the Assistant Varsity Coach for Northwood High School for two years, privately coached tennis for about 10 years and have coached at numerous tennis academies. This past year (2022-2023) I had one student win CIF Division 1 (Portola High School), another win CIF Division 2 (Sage Hill), and another was a finalist of CIF Division 2 (J. Serra) so I’m confident that my student’s experience success at the highest high school level. I pride myself in having 100% success rate of students entering the high school tennis team (Frosh, JV, Varsity).",
+            "bio": "Hello my name is Derek and I am a local Irvine tennis coach with over 20 years of playing experience and over 16 years of teaching experience! I achieved a Top 75 national ranking back in juniors. I was the #1 singles player for the national club team at UC Davis (which is my alma mater and after graduating, I also played on the Irvine Valley College’s 3-Time State Championship Team). I was the Assistant Varsity Coach for Northwood High School for two years, privately coached tennis for about 10 years and have coached at numerous tennis academies. This past year (2022-2023) I had one student win CIF Division 1 (Portola High School), another win CIF Division 2 (Sage Hill), and another was a finalist of CIF Division 2 (J. Serra) so I’m confident that my student’s experience success at the highest high school level. I pride myself in having 100% success rate of students entering the high school tennis team (Frosh, JV, Varsity). I'm fluent in Chinese too if you prefer that instead. Hope to hear from you!",
             "location": "Irvine",
             "image_url": "/images/coach_profile_image/Coach-Derek-Lin.jpeg",
             "experience_years": 16,
@@ -20,7 +20,7 @@ def seed_coaches_and_availabilities():
             "first_name": "Rouzbeh",
             "last_name": "Kamran",
             "rate": 150,
-            "bio": "My name Coach Rouzbeh and I am a professional tennis player from Iran.",
+            "bio": "My name Coach Rouzbeh and I am a professional tennis player from Iran. I have played in the Davis Cup representing Iran, and was the #1 player in Iran for many years. I now live in Irvine and have coached all kinds of students to the highest level. One of my students recently got a full ride to play Division 1 at UPenn. I speak Arabic as well and look forward to working with you.",
             "location": "Irvine",
             "image_url": "/images/coach_profile_image/Coach-Rouzbeh-Kamran.jpeg",
             "experience_years": 30,
@@ -36,7 +36,7 @@ def seed_coaches_and_availabilities():
         {
             "first_name": "Roger",
             "last_name": "Federer",
-            "rate": 1000,
+            "rate": 100000000,
             "bio": "Hi, I’m Roger Federer, a professional tennis player with over two decades of experience competing at the highest level. Throughout my career, I’ve won 20 Grand Slam singles titles and have held the No. 1 spot in the ATP rankings for a record 310 weeks. My passion for tennis goes beyond competition—I love sharing my knowledge and expertise with others. Whether you're just starting out or looking to refine your skills, I’m excited to help you elevate your game on the court. I also speak Swiss, German, and French and can coach in those languages if you prefer.",
             "location": "San Francisco",
             "image_url": "/images/coach_profile_image/Coach-Roger-Federer.jpeg",
@@ -64,7 +64,7 @@ def seed_coaches_and_availabilities():
         {
             "first_name": "Carlos",
             "last_name": "Alcaraz",
-            "rate": 1500,
+            "rate": 150000,
             "bio": "Hola! My name is Carlos Alcaraz, and I am a professional tennis player from Spain. As the youngest player to reach the No. 1 spot in the ATP rankings, I have had the privilege of competing on the biggest stages in the world, including winning my first Grand Slam title at the US Open. With a powerful baseline game and relentless energy, I aim to inspire and mentor the next generation of tennis stars. Whether you're just starting or looking to refine your advanced skills, I am excited to help you reach your full potential on the court. I speak Spanish and would love to coach in both Spanish and English!",
             "location": "San Jose",
             "image_url": "/images/coach_profile_image/Coach-Carlos-Alcaraz.jpeg",
@@ -107,8 +107,107 @@ def seed_coaches_and_availabilities():
                 {"day_of_week": "Friday", "start_time": "08:00", "end_time": "12:00"},
                 {"day_of_week": "Saturday", "start_time": "08:00", "end_time": "12:00"},
                 {"day_of_week": "Sunday", "start_time": "08:00", "end_time": "12:00"},
+            ],
+            
+        },
+        {
+            "first_name": "Jannik",
+            "last_name": "Sinner",
+            "rate": 2000,
+            "bio": "Hi, I’m Jannik Sinner, a professional tennis player from Italy. At a young age, I’ve already made a significant impact on the ATP Tour, reaching the quarterfinals or better in all Grand Slam tournaments and winning multiple ATP titles. Known for my powerful groundstrokes and mental toughness, I’ve been ranked as high as World No. 4 and continue to rise in the tennis world. Whether you're looking to refine your technique or take your game to the next level, I’m excited to help you reach your goals on the court. I speak both Italian and English, and I’m happy to coach in either language.",
+            "location": "Los Angeles",
+            "image_url": "/images/coach_profile_image/Coach-Jannik-Sinner.jpeg",
+            "experience_years": 10,
+            "availabilities": [
+                {"day_of_week": "Monday", "start_time": "12:00", "end_time": "16:00"},
+                {"day_of_week": "Wednesday", "start_time": "12:00", "end_time": "16:00"},
+                {"day_of_week": "Friday", "start_time": "12:00", "end_time": "16:00"}
             ]
         },
+        {
+            "first_name": "Rafael",
+            "last_name": "Nadal",
+            "rate": 2500,
+            "bio": "Hola! I’m Rafael Nadal, a professional tennis player from Spain with over 20 Grand Slam titles to my name. Known as the 'King of Clay,' I've spent decades at the top of the ATP rankings. My experience on the court and unmatched mental toughness have brought me success on all surfaces. Whether you're an advanced player looking to master your technique or a beginner, I'm excited to help you excel in your tennis journey. I’m fluent in both Spanish and English, and I’m happy to coach in either language.",
+            "location": "Irvine",
+            "image_url": "/images/coach_profile_image/Coach-Rafael-Nadal.jpeg",
+            "experience_years": 22,
+            "availabilities": [
+                {"day_of_week": "Monday", "start_time": "08:00", "end_time": "12:00"},
+                {"day_of_week": "Wednesday", "start_time": "08:00", "end_time": "12:00"},
+                {"day_of_week": "Friday", "start_time": "08:00", "end_time": "12:00"}
+            ]
+        },
+        {
+            "first_name": "Andy",
+            "last_name": "Roddick",
+            "rate": 1500,
+            "bio": "Hey there! I’m Andy Roddick, a former World No. 1 tennis player from the USA. I was known for my powerful serve and competitive spirit, winning the US Open in 2003. I bring years of experience at the highest levels of the game and look forward to sharing my knowledge with players of all skill levels. Whether you're looking to improve your serve or sharpen your overall game, I’m here to help. I’m also an engaging instructor who loves making tennis fun.",
+            "location": "Los Angeles",
+            "image_url": "/images/coach_profile_image/Coach-Andy-Roddick.jpeg",
+            "experience_years": 20,
+            "availabilities": [
+                {"day_of_week": "Tuesday", "start_time": "12:00", "end_time": "16:00"},
+                {"day_of_week": "Thursday", "start_time": "12:00", "end_time": "16:00"},
+                {"day_of_week": "Saturday", "start_time": "12:00", "end_time": "16:00"}
+            ]
+        },
+        {
+            "first_name": "Denis",
+            "last_name": "Shapovalov",
+            "rate": 1200,
+            "bio": "Hi, I’m Denis Shapovalov, a professional tennis player from Canada. I’ve been recognized for my aggressive game style and flashy one-handed backhand. I’ve competed at the highest levels of the ATP Tour and am excited to share my insights on modern tennis techniques. Whether you’re a competitive player or just looking to improve your skills, I’m here to help. I also speak Russian and would love to coach in either English or Russian.",
+            "location": "San Francisco",
+            "image_url": "/images/coach_profile_image/Coach-Denis-Shapovalov.jpeg",
+            "experience_years": 8,
+            "availabilities": [
+                {"day_of_week": "Monday", "start_time": "10:00", "end_time": "14:00"},
+                {"day_of_week": "Tuesday", "start_time": "10:00", "end_time": "14:00"},
+                {"day_of_week": "Friday", "start_time": "10:00", "end_time": "14:00"}
+            ]
+        },
+        {
+            "first_name": "Grigor",
+            "last_name": "Dimitrov",
+            "rate": 1100,
+            "bio": "Hello! I’m Grigor Dimitrov, a professional tennis player from Bulgaria. Throughout my career, I’ve been known for my smooth all-court game and elegant one-handed backhand. I’ve reached the semifinals in multiple Grand Slam tournaments and competed against the top players in the world. Whether you're a junior player or an adult looking to refine your game, I’m here to help you improve every aspect of your play. I speak both Bulgarian and English, so feel free to choose whichever language you're comfortable with!",
+            "location": "Irvine",
+            "image_url": "/images/coach_profile_image/Coach-Grigor-Dimitrov.jpeg",
+            "experience_years": 12,
+            "availabilities": [
+                {"day_of_week": "Tuesday", "start_time": "08:00", "end_time": "12:00"},
+                {"day_of_week": "Thursday", "start_time": "08:00", "end_time": "12:00"},
+                {"day_of_week": "Saturday", "start_time": "08:00", "end_time": "12:00"}
+            ]
+        },
+        {
+            "first_name": "Daniil",
+            "last_name": "Medvedev",
+            "rate": 1800,
+            "bio": "Hi, I’m Daniil Medvedev, a professional tennis player from Russia. Known for my counter-punching style and incredible defense, I’ve been a mainstay in the top rankings of the ATP Tour. I’ve won multiple Masters 1000 titles and a Grand Slam, and I’m eager to share my expertise with players of all skill levels. I’m fluent in Russian and English, and I’d be happy to coach in either language.",
+            "location": "San Jose",
+            "image_url": "/images/coach_profile_image/Coach-Daniil-Medvedev.jpeg",
+            "experience_years": 12,
+            "availabilities": [
+                {"day_of_week": "Monday", "start_time": "09:00", "end_time": "13:00"},
+                {"day_of_week": "Wednesday", "start_time": "09:00", "end_time": "13:00"},
+                {"day_of_week": "Friday", "start_time": "09:00", "end_time": "13:00"}
+            ]
+        },
+        {
+            "first_name": "Naomi",
+            "last_name": "Osaka",
+            "rate": 1700,
+            "bio": "Hi, I’m Naomi Osaka, a professional tennis player from Japan. I’ve won four Grand Slam titles and have been ranked as high as World No. 1. I’m passionate about mental health and empowering others both on and off the court. Whether you’re just starting out or aiming to compete at a high level, I’m excited to work with you on your journey. I’m fluent in both Japanese and English and can coach in either language.",
+            "location": "San Jose",
+            "image_url": "/images/coach_profile_image/Coach-Naomi-Osaka.jpeg",
+            "experience_years": 9,
+            "availabilities": [
+                {"day_of_week": "Tuesday", "start_time": "10:00", "end_time": "14:00"},
+                {"day_of_week": "Thursday", "start_time": "10:00", "end_time": "14:00"},
+                {"day_of_week": "Saturday", "start_time": "10:00", "end_time": "14:00"}
+            ]
+        }
         
     ]
 
--- frontend/src/components/CoachProfile/CoachProfile.jsx
+++ frontend/src/components/CoachProfile/CoachProfile.jsx
@@ -1,156 +1,201 @@
-import { useParams } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useMemo } from 'react';
-import { fetchCoachById } from '../../redux/coach';
-import { useModal } from '../../context/Modal';
-import { fetchAvailabilityThunk, clearAvailability } from '../../redux/availability';
-import { fetchReviewsByCoachThunk, deleteReviewThunk } from '../../redux/review';
-import CoachAvailabilityModal from '../CoachAvailabilityModal';
-import BookLessonModal from '../BookLessonModal';
-import ReviewModal from '../ReviewModal';
-import UpdateReviewModal from '../UpdateReviewModal.jsx';
-import './CoachProfile.css';
+import { useEffect, useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { useModal } from "../../context/Modal";
+import {
+  clearAvailability,
+  fetchAvailabilityThunk,
+} from "../../redux/availability";
+import { fetchCoachById } from "../../redux/coach";
+import {
+  deleteReviewThunk,
+  fetchReviewsByCoachThunk,
+} from "../../redux/review";
+import BookLessonModal from "../BookLessonModal";
+import CoachAvailabilityModal from "../CoachAvailabilityModal";
+import ReviewModal from "../ReviewModal";
+import UpdateReviewModal from "../UpdateReviewModal.jsx";
+import "./CoachProfile.css";
 
 function CoachProfile() {
-    const { coachId } = useParams();
-    const dispatch = useDispatch();
-    const { setModalContent, closeModal } = useModal();
+  const { coachId } = useParams();
+  const dispatch = useDispatch();
+  const { setModalContent, closeModal } = useModal();
 
-    const coach = useSelector(state => state.coach.coaches[coachId]);
-    const reviews = useSelector(state => state.reviews.coachReviews); // Select reviews from Redux state
-    const currentUser = useSelector(state => state.session.user);
+  const coach = useSelector((state) => state.coach.coaches[coachId]);
+  const reviews = useSelector((state) => state.reviews.coachReviews); // Select reviews from Redux state
+  const currentUser = useSelector((state) => state.session.user);
 
-    useEffect(() => {
-        dispatch(fetchCoachById(coachId));
-        dispatch(fetchAvailabilityThunk(coachId));
-        dispatch(fetchReviewsByCoachThunk(coachId)); // Fetch reviews by coach
+  useEffect(() => {
+    dispatch(fetchCoachById(coachId));
+    dispatch(fetchAvailabilityThunk(coachId));
+    dispatch(fetchReviewsByCoachThunk(coachId));
 
-        return () => {
-            dispatch(clearAvailability(coachId)); // Clear availability on unmount
-        };
-    }, [dispatch, coachId]);
-
-    const handleViewAvailability = async (coach) => {
-        await dispatch(fetchAvailabilityThunk(coach.id));
-        setModalContent(
-            <CoachAvailabilityModal
-                coach={coach}
-                onClose={() => {
-                    dispatch(clearAvailability(coach.id));
-                    closeModal();
-                }}
-            />
-        );
+    return () => {
+      dispatch(clearAvailability(coachId));
     };
+  }, [dispatch, coachId]);
 
-    const handleBookLesson = (coach) => {
-        setModalContent(
-            <BookLessonModal
-                coach={coach}
-                initialLesson={null}
-                onClose={closeModal}
-            />
-        );
-    };
-
-    const handleWriteReview = (coach) => {
-        setModalContent(
-            <ReviewModal
-                coach={coach}
-                onClose={closeModal}
-            />
-        );
-    };
-
-    const handleUpdateReview = (review) => {
-        setModalContent(
-            <UpdateReviewModal
-                review={review}
-                onClose={closeModal}
-            />
-        );
-    };
-
-    const handleDeleteReview = (reviewId) => {
-        const confirmation = window.confirm("Are you sure you want to delete this review?");
-        if (confirmation) {
-            dispatch(deleteReviewThunk(reviewId));
-        }
-    };
-
-    // Calculate the total number of reviews and the average rating
-    const { totalReviews, averageRating } = useMemo(() => {
-        const reviewsArray = Object.values(reviews);
-        const totalReviews = reviewsArray.length;
-        const averageRating = totalReviews > 0
-            ? (reviewsArray.reduce((acc, review) => acc + review.rating, 0) / totalReviews).toFixed(1)
-            : 0;
-        return { totalReviews, averageRating };
-    }, [reviews]);
-
-    if (!coach) {
-        return <div>Loading...</div>;
-    }
-
-    return (
-        <div className="coach-profile-container">
-            <div className="coach-profile-content">
-                <div className="coach-profile-left">
-                    <img src={coach.image_url} alt={`Coach ${coach.first_name}`} className="coach-profile-image" />
-                    <div className="coach-profile-details">
-                        <p><strong>Rate:</strong> ${coach.rate} / hour</p>
-                        <p><strong>{coach.experience_years}+ years of experience</strong></p>
-                        <button className="profile-btn view-availability-btn" onClick={() => handleViewAvailability(coach)}>View Availability</button>
-                        <button className="profile-btn book-lesson-btn" onClick={() => handleBookLesson(coach)}>Book A Lesson!</button>
-                        <button className="profile-btn write-review-btn" onClick={() => handleWriteReview(coach)}>Write a Review!</button>
-                    </div>
-                </div>
-                <div className="coach-profile-right">
-                    <div className="coach-profile-header">
-                        <h1>Coach {coach.first_name} {coach.last_name}</h1>
-                        <h2>Located in {coach.location}, California</h2>
-                    </div>
-                    <h3>Bio:</h3>
-                    <p>{coach.bio}</p>
-
-                    <div className="coach-reviews-summary">
-                        <p>
-                            <strong>
-                                {totalReviews} {totalReviews === 1 ? 'Review' : 'Reviews'}
-                                {totalReviews > 0 && (
-                                    <> · 🎾 {averageRating} </>
-                                )}
-                            </strong>
-                        </p>
-                    </div>
-
-                    <div className="coach-reviews">
-                        <h3>Reviews:</h3>
-                        {totalReviews === 0 ? (
-                            <p>No reviews yet.</p>
-                        ) : (
-                            Object.values(reviews)
-                                .sort((a, b) => new Date(b.created_at) - new Date(a.created_at)) // Sort by most recent
-                                .map(review => (
-                                    <div key={review.id} className="review-item">
-                                        <p><strong>{review.user.username}:</strong> {review.comment}</p>
-                                        <p><strong>Rating:</strong> {review.rating} 🎾</p>
-                                        <p className="review-date"><em>Posted on {new Date(review.created_at).toLocaleDateString('en-US', { year: 'numeric', month: 'long' })}</em></p>
-
-                                        {currentUser && currentUser.id === review.user_id && (
-                                            <div className="review-actions">
-                                                <button id='review-update-btn' onClick={() => handleUpdateReview(review)}>Update</button>
-                                                <button onClick={() => handleDeleteReview(review.id)}>Delete</button>
-                                            </div>
-                                        )}
-                                    </div>
-                                ))
-                        )}
-                    </div>
-                </div>
-            </div>
-        </div>
+  const handleViewAvailability = async (coach) => {
+    await dispatch(fetchAvailabilityThunk(coach.id));
+    setModalContent(
+      <CoachAvailabilityModal
+        coach={coach}
+        onClose={() => {
+          dispatch(clearAvailability(coach.id));
+          closeModal();
+        }}
+      />
     );
+  };
+
+  const handleBookLesson = (coach) => {
+    setModalContent(
+      <BookLessonModal
+        coach={coach}
+        initialLesson={null}
+        onClose={closeModal}
+      />
+    );
+  };
+
+  const handleWriteReview = (coach) => {
+    setModalContent(<ReviewModal coach={coach} onClose={closeModal} />);
+  };
+
+  const handleUpdateReview = (review) => {
+    setModalContent(<UpdateReviewModal review={review} onClose={closeModal} />);
+  };
+
+  const handleDeleteReview = (reviewId) => {
+    const confirmation = window.confirm(
+      "Are you sure you want to delete this review?"
+    );
+    if (confirmation) {
+      dispatch(deleteReviewThunk(reviewId));
+    }
+  };
+
+  // Calculate the total number of reviews and the average rating
+  const { totalReviews, averageRating } = useMemo(() => {
+    const reviewsArray = Object.values(reviews);
+    const totalReviews = reviewsArray.length;
+    const averageRating =
+      totalReviews > 0
+        ? (
+            reviewsArray.reduce((acc, review) => acc + review.rating, 0) /
+            totalReviews
+          ).toFixed(1)
+        : 0;
+    return { totalReviews, averageRating };
+  }, [reviews]);
+
+  if (!coach) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="coach-profile-container">
+      <div className="coach-profile-content">
+        <div className="coach-profile-left">
+          <img
+            src={coach.image_url}
+            alt={`Coach ${coach.first_name}`}
+            className="coach-profile-image"
+          />
+          <div className="coach-profile-details">
+            <p>
+              <strong>Rate:</strong> ${Number(coach.rate).toLocaleString()} /
+              hour
+            </p>
+            <p>
+              <strong>{coach.experience_years}+ years of experience</strong>
+            </p>
+            <button
+              className="profile-btn view-availability-btn"
+              onClick={() => handleViewAvailability(coach)}
+            >
+              View Availability
+            </button>
+            <button
+              className="profile-btn book-lesson-btn"
+              onClick={() => handleBookLesson(coach)}
+            >
+              Book A Lesson!
+            </button>
+            <button
+              className="profile-btn write-review-btn"
+              onClick={() => handleWriteReview(coach)}
+            >
+              Write a Review!
+            </button>
+          </div>
+        </div>
+        <div className="coach-profile-right">
+          <div className="coach-profile-header">
+            <h1>
+              Coach {coach.first_name} {coach.last_name}
+            </h1>
+            <h2>Located in {coach.location}, California</h2>
+          </div>
+          <h3>Bio:</h3>
+          <p>{coach.bio}</p>
+
+          <div className="coach-reviews-summary">
+            <p>
+              <strong>
+                {totalReviews} {totalReviews === 1 ? "Review" : "Reviews"}
+                {totalReviews > 0 && <> · 🎾 {averageRating} </>}
+              </strong>
+            </p>
+          </div>
+
+          <div className="coach-reviews">
+            <h3>Reviews:</h3>
+            {totalReviews === 0 ? (
+              <p>No reviews yet.</p>
+            ) : (
+              Object.values(reviews)
+                .sort((a, b) => new Date(b.created_at) - new Date(a.created_at)) // Sort by most recent
+                .map((review) => (
+                  <div key={review.id} className="review-item">
+                    <p>
+                      <strong>{review.user.username}:</strong> {review.comment}
+                    </p>
+                    <p>
+                      <strong>Rating:</strong> {review.rating} 🎾
+                    </p>
+                    <p className="review-date">
+                      <em>
+                        Posted on{" "}
+                        {new Date(review.created_at).toLocaleDateString(
+                          "en-US",
+                          { year: "numeric", month: "long" }
+                        )}
+                      </em>
+                    </p>
+
+                    {currentUser && currentUser.id === review.user_id && (
+                      <div className="review-actions">
+                        <button
+                          id="review-update-btn"
+                          onClick={() => handleUpdateReview(review)}
+                        >
+                          Update
+                        </button>
+                        <button onClick={() => handleDeleteReview(review.id)}>
+                          Delete
+                        </button>
+                      </div>
+                    )}
+                  </div>
+                ))
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CoachProfile;
